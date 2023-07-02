0ad7a270baf0bc53211505d2a48bda49062f2eac
“Your <ducanh2kqo@gmail.com>
2023-07-02 22:54:08 +0700

    video5 multiple components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+    <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Hello Đức Anh đẹp trai</h2>
       </header>
     </div>
   );
